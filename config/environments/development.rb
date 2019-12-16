cb7168be6765b2caa7da7bf6639d27208dd2fe05
deguzman20 <alejandrogacumadeguzman@gmail.com>
2019-12-16 10:49:55 +0800

    Fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp", "caching-dev.txt").exist?
@@ -26,7 +26,6 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
