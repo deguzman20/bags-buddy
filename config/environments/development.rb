3cf2c7476684353c7de0b3ccd389d278fa7ef605
deguzman20 <alejandrogacumadeguzman@gmail.com>
2019-12-16 10:43:43 +0800

    Add uglifier

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
