9a5b8b2c1754e4c48b43cf3227c52500301d3d81
deguzman20 <alejandrogacumadeguzman@gmail.com>
2019-12-16 15:46:50 +0800

    Add admin role

--- db/migrate/20190811095123_devise_create_users.rb
+++ db/migrate/20190811095123_devise_create_users.rb
@@ -10,7 +10,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.datetime :birthdate
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.boolean :admin 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
